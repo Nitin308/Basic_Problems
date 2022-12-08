3e7cbea9745b7ac937a5e707faf36f6e04ee8e46
Nitin308 <nitin3081998@gmail.com>
2022-12-08 12:56:38 +0530

    Compute Quotient and Remainder

---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,20 @@
+﻿class Swap2Num
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter value of a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter value of b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int temp;
+
+        Console.WriteLine("Before Swapping: Value of a={0}, b={1}", a, b);
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("After Swapping: Value of a={0}, b={1}", a, b);
+        Console.ReadLine();
+    }
+}
