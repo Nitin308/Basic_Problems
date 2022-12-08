2ee110b174f433d777095c1e6edc432c9b56fe31
Nitin308 <nitin3081998@gmail.com>
2022-12-08 12:36:20 +0530

    Print Table of the Powers of 2

---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿class Powerof2
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the power value: ");
+        long N = Convert.ToInt32(Console.ReadLine());
+        int num = 2;
+        for (var i = 0; i < N; i++)
+        {
+            Console.WriteLine(num);
+            num = num * 2;
+        }
+        Console.ReadLine();
+    }
+}
