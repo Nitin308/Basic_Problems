ba4aa668447db53698891a80bd4913916e158363
Nitin308 <nitin3081998@gmail.com>
2022-12-08 11:55:50 +0530

    Harmonic Number

---
+++ BasicCoreProblems/Basic_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿class HarmonicNumber
+{
+    static void Main(String[] args)
+    {
+        float count;
+        float sum = 0;
+
+        Console.WriteLine("Enter the value of N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        for (count = 1; count <= N; count++)
+        {
+            Console.WriteLine("1/{0} + ", count);
+            sum += 1 / count;
+        }
+        Console.WriteLine("Sum of series of {0} terms: {1}", N, sum);
+        Console.ReadLine();
+    }
+}
