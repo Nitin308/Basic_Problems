fe5e5f59c5880d7a8076e8070cd8c67ab0933461
Nitin308 <nitin3081998@gmail.com>
2022-12-08 12:03:54 +0530

    Find the Largest among Three Numbers

---
+++ BasicCoreProblem/BasicCoreProblems/Program.cs
@@ -0,0 +1,26 @@
+﻿class LargestNum
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter a value for x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a value for y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a value for z: ");
+        int z = Convert.ToInt32(Console.ReadLine());
+
+        if (x > y && x > z)
+        {
+            Console.WriteLine("x is greater");
+        }
+        else if (y > x && y > z)
+        {
+            Console.WriteLine("y is greater");
+        }
+        else
+        {
+            Console.WriteLine("z is greater");
+        }
+        Console.ReadLine();
+    }
+}
