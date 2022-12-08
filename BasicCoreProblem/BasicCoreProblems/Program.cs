816dd26a46d06fc3c19a7653b698e6a66397f19a
Nitin308 <nitin3081998@gmail.com>
2022-12-08 12:12:09 +0530

    The input year is a Leap Year or not

---
+++ BasicCoreProblem/BasicCoreProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿class LeapYear
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the Year in 4 digits: ");
+        int Year = Convert.ToInt32(Console.ReadLine());
+
+        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+        {
+            Console.WriteLine("{0} is a Leap Year", Year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a Leap Year", Year);
+        }
+        Console.ReadLine();
+    }
+}
