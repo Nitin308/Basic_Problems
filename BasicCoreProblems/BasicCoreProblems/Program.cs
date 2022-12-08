112bed239f70860437a18d20056ea44112b24088
Nitin308 <nitin3081998@gmail.com>
2022-12-08 11:28:35 +0530

    Compute Prime Factors of number N

---
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -0,0 +1,22 @@
+﻿class Factors
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter a value: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+
+        for (i = 1; i <= N; i++)
+        {
+            if (N % i == 0)
+            {
+                Console.WriteLine(i + " is a Factor of " + N);
+            }
+            else
+            {
+                Console.WriteLine(i + " is not a Factor of " + N);
+            }
+        }
+        Console.ReadLine();
+    }
+}
