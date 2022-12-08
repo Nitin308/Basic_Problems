11fbcbc5029918bf6ed7520bddc11ba1f62eb841
Nitin308 <nitin3081998@gmail.com>
2022-12-08 12:46:05 +0530

    Compute Quotient and Remainder

---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿class QuotientAndRemainder
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter a Dividend: ");
+        int dividend = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a Divisor: ");
+        int divisor = Convert.ToInt32(Console.ReadLine());
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        Console.WriteLine("Quotient = " + quotient);
+        Console.WriteLine("Remainder = " + remainder);
+        Console.ReadLine();
+    }
+}
